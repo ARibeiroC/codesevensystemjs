cbf9c6fc46abe9d2c48dee35f44a8c94547bccd7
Anderson F. Ribeiro Campos <andersonspl@hotmail.com>
2025-01-14 02:29:50 -0300

    fix: problema de cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { connection } from './db/conn.js'
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors(`http://localhost:3000/`))
+app.use(cors(`http://localhost:${port}/`))
 
 
 connection()
